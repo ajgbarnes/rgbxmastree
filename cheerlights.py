60c14ea67c298f47ff593df1da18559b8dc9b7b6
ajgbarnes <ajgbarnes@gmail.com>
2020-12-13 20:24:48 +0000

    Tidy up

--- cheerlights.py
+++ cheerlights.py
@@ -2,7 +2,7 @@ from tree import RGBXmasTree
 from colorzero import Color
 import paho.mqtt.client as mqtt
 
-bright=0.25
+bright=0.1
 
 def on_connect(client, obj, flags, rc):
     print("Connected with result code "+str(rc))
@@ -12,7 +12,6 @@ def on_message(client, obj, msg):
     colorValue = msg.payload.decode('UTF-8')
     print(colorValue)
     tree.color=Color(colorValue)
-    print(msg.topic)
 
 def on_log(client, obj, level, msg):
     print(msg)
@@ -25,6 +24,5 @@ client.on_message = on_message
 #client.on_log = on_log
 
 client.connect("mqtt.cheerlights.com", 1883, 60)
-client.subscribe("cheerlightsRGB")
 
 client.loop_forever()
